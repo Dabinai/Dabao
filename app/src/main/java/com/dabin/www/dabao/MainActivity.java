c77d03ac8e9acba2f906c4b91080a4719365157e
Dabin <820040739@qq.com>
2017-11-12 19:22:32 +0800

    Initial commit

---
+++ app/src/main/java/com/dabin/www/dabao/MainActivity.java
@@ -0,0 +1,32 @@
+package com.dabin.www.dabao;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //弹出打包签名
+    public void strat(View view){
+        String channel = getChannel();
+        Toast.makeText(MainActivity.this,channel,Toast.LENGTH_SHORT).show();
+    }
+    private String getChannel() {
+        try {
+            PackageManager pm = getPackageManager();
+            ApplicationInfo appInfo = pm.getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            return appInfo.metaData.getString("UMENG_CHANNEL");
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return "";
+    }
+}
